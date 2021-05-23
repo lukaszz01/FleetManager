ebb3f77e03273ddc325d7c359bfb138e461e71b1
maniek1998 <marcin1998119@gmail.com>
2021-05-23 14:31:55 +0200

    2305_1431

--- SBBD/RouteReport.cs
+++ SBBD/RouteReport.cs
@@ -99,8 +99,50 @@ namespace SBBD
                 Document doc = new Document();
                 PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
                 doc.Open();
-                //tutaj kod na tabele
-                
+                if (vehicleId != -1)
+                {
+                    var vehicle = context.Vehicles.Where(v => v.vehicle_id == vehicleId).FirstOrDefault();
+                    var vehicleRoutes = context.Vehicles_Routes.Where(r => r.vehicle_id == vehicleId).ToList();
+                    Drivers routeDriver;
+                    int? distanceSum = 0;
+                    //int days = 0;
+                    int totalDays = 0;
+                    PdfPTable table = new PdfPTable(5);
+                    PdfPCell cell1 = new PdfPCell(new Phrase($"{vehicle.manufacturer} {vehicle.model} {vehicle.registration_num}"));
+                    cell1.Colspan = 5;
+                    cell1.HorizontalAlignment = 1;
+                    table.AddCell(cell1);
+                    PdfPCell cell2 = new PdfPCell(new Phrase("Imie i Nazwisko kierowcy"));
+                    cell2.Colspan = 2;
+                    cell2.HorizontalAlignment = 1;
+                    table.AddCell(cell2);
+                    table.AddCell("Przebyty dystans");
+                    table.AddCell("Data wyjazdu");
+                    table.AddCell("Data Powrotu");
+                    foreach (Vehicles_Routes route in vehicleRoutes)
+                    {
+                        if (route.end_date != null)
+                        {
+                            distanceSum += route.distance;
+                            routeDriver = context.Drivers.Where(d => d.driver_id == route.driver_id).FirstOrDefault();
+                            table.AddCell(routeDriver.first_name);
+                            table.AddCell(routeDriver.last_name);
+                            table.AddCell(route.distance.ToString());
+                            table.AddCell(route.start_date.ToString("dd-MM-yyyy"));
+                            table.AddCell(route.end_date == null ? "w trasie" : route.end_date?.ToString("dd-MM-yyyy"));
+                            //days = Convert.ToInt32(((route.end_date ?? DateTime.Now) - route.start_date).TotalDays);
+                            totalDays += Convert.ToInt32(((route.end_date ?? DateTime.Now) - route.start_date).TotalDays) + 1;//days == 0 ? 1 : days;
+                        }
+                    }
+                    PdfPCell cell3 = new PdfPCell(new Phrase("Suma dystansu"));
+                    cell3.Colspan = 2;
+                    table.AddCell(cell3);
+                    table.AddCell(distanceSum.ToString());
+                    table.AddCell("Dni w trasie");
+                    table.AddCell(totalDays.ToString());
+                    doc.Add(table);
+                }
+
                 doc.Close();
             }
         }
